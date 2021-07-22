078e85b52d8459d4ccbb1085317d6de0b21efc2c
harsh116 <71572299+harsh116@users.noreply.github.com>
2021-07-22 10:29:26 +0530

    change ssl config

--- database/knex.js
+++ database/knex.js
@@ -3,7 +3,7 @@ const knex = require("knex")({
   connection: {
     // host: "127.0.0.1",
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
     
     
     
