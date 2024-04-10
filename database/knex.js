7e3bf3dd8b8fa5f2f3e6d1696d7c39cfeb26b2f4
alex <you@example.com>
2024-04-10 05:44:54 +0530

    changed remote postgresql from  elephantsql to neon

--- database/knex.js
+++ database/knex.js
@@ -4,7 +4,8 @@ const knex = require("knex")({
     // host: "127.0.0.1",
     connectionString:
       process.env.DATABASE_URL ||
-      "postgres://uqvfyumt:hx08eQNWBRat-Pe6apYj6VElMTGYDYjh@arjuna.db.elephantsql.com/uqvfyumt",
+      // "postgres://uqvfyumt:hx08eQNWBRat-Pe6apYj6VElMTGYDYjh@arjuna.db.elephantsql.com/uqvfyumt",
+      "postgresql://neondb1_owner:Q3qrfKDxlwh0@ep-late-sea-a2u4c2qe.eu-central-1.aws.neon.tech/neondb1",
     ssl: { rejectUnauthorized: false },
   },
 });
